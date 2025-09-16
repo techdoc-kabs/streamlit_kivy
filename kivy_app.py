7398d50c8dd66a506052f11f0f9e137013d21b9f
techdoc-kabs <techdoc.kabs@gmail.com>
2025-09-16 19:14:06 +0300

    Update kivy_app.py

--- kivy_app.py
+++ kivy_app.py
@@ -27,6 +27,11 @@ st.markdown("""
 @media (max-width:992px){.card{flex:0 0 48%;}}
 @media (max-width:480px){.card{flex:0 0 48%; padding:10px; min-width:120px;}}
 @media (max-width:350px){.card{flex:0 0 120%; padding:10px; min-width:120px;}}
+@media (max-width: 480px){
+  .cards-wrap {
+    justify-content: center;  /* center cards horizontally */
+  }
+}
 </style>
 """, unsafe_allow_html=True)
 
@@ -76,6 +81,7 @@ else:
         st.session_state.clicked_card = None
         st.experimental_set_query_params()
         st.rerun()
+
 
 
 
